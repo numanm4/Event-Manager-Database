58f75f08840528870560eeaf4ebc47140bd16500
Numan Muhammad <133197082+numanm4@users.noreply.github.com>
2023-12-29 20:46:02 -0700

    sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -3,22 +3,22 @@ const nodemailer = require('nodemailer');
 const notifyAdmin = async (recipients, subject, message) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
+            host: 'smtp.gmail.com', // or any other email provider 
             port: 465,
             secure: true,
             auth: {
-                user: 'numanbnr4@gmail.com',
-                pass: 'tanh uxls dshx dkpb' 
+                user: ' ', // use your email address
+                pass: ' ' // use with your email 2 step verification password 
             },
             tls: {
-                rejectUnauthorized: false
+                rejectUnauthorized: false // made false for smooth functionality, change for more security
             }
         });
 
         const listOfRecipients = recipients.split(',').map(email => email.trim());
 
         const info = await transporter.sendMail({
-            from: 'numanbnr4@gmail.com',
+            from: ' ', // use your email address
             to: listOfRecipients.join(', '), 
             subject: subject,
             html: message
